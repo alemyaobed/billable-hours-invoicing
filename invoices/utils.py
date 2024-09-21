d3cda3ec0622a5f095b0c91ce2543bbbe51131d7
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-09-21 17:15:03 +0000

    worked on the front end better

--- invoices/utils.py
+++ invoices/utils.py
@@ -10,6 +10,6 @@ def convert_decimal_to_string(data):
     elif isinstance(data, list):
         return [convert_decimal_to_string(v) for v in data]
     elif isinstance(data, Decimal):
-        return str(data)
+        return f"{data:.2f}"
     else:
         return data
